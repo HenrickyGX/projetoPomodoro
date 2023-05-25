10ed23f67c2f1e295f86733e17d91542b0df2e66
HenrickyGX <henrysampm007@outlook.com>
2023-05-25 11:52:54 -0400

    alteração

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {   Flex, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [time, setTime] = useState(0);
+  const [timerStart, setTimerStart] = useState(false);
+ 
 
+  const buttons = [
+    {
+      value: 60,
+      display: "1 minutes"
+    },
+    {
+      value: 900,
+      display: "15 minutes",
+    },
+    {
+      value: 1800,
+      display: "30 minutes",
+    },
+    {
+      value: 3600,
+      display: "60 minutes",
+    },
+    {
+      value: 0,
+      display: "Restart",
+
+    }
+  ];
+  const toggleTimer = () => {
+    setTimerStart(!timerStart);
+  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (timerStart) {
+        if (time > 0) {
+          setTime(time - 1);
+        } else if (time === 0) {
+          
+          clearInterval(interval);
+        }
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [timerStart, time]);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>PROJETO POMODORO</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="App" style={{ height: "100%" }}>
+      <Flex
+        background="gray.700"
+        height="100%"
+        alignItems="center"
+        flexDirection="column"
+      >
+        <h1>
+         Tempo Pomodoro
+        </h1>
+        <Text fontWeight="bold" fontSize="7xl" color="white">
+          {`${Math.floor(time / 60) < 10
+              ? `0${Math.floor(time / 60)}`
+              : `${Math.floor(time / 60)}`
+            }:${time % 60 < 10 ? `0${time % 60}` : time % 60}`}
+        </Text>
+        <Flex>
+          <button onClick={toggleTimer}>
+            {!timerStart ? "Start" : "Pause"}
+          </button>
 
-export default App
+
+        </Flex>
+        <Flex marginTop={10}>
+          {buttons.map(({ value, display }) => (
+            <button
+              color="white"
+              onClick={() => {
+                setTimerStart(false);
+                setTime(value);
+              }}
+            >
+              {display}
+            </button>
+          ))}
+        </Flex>
+      </Flex>
+    </div>
+  );
+}
+export default App;
